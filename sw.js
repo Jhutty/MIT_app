486de0a97a1931ac8a92641495fa4245c50ae20c
Suneet Singh Jhutty <jhutty@protonmail.com>
2026-06-22 21:46:26 +0200

    add MIT app

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('mit-v1').then(function(cache) {
+                                return cache.addAll(["./mit-app.html", "./manifest.json", "icon-192.png", "icon-512.png"])
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+  caches.match(event.request).then(function(response) {
+    return response || fetch(event.request);
+  })
+);
+
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.filter(function(n) { return n !== 'mit-v1'; }).map(function(n) { return caches.delete(n); }))
+    })
+  );
+});
+
